e28ac91125772bf919ebb40d62aea895d3ad2bbe
cristianogle <cristianogelli@gmail.com>
2016-06-16 15:08:52 +0200

    Add files via upload
    aggiunto check.php (provvisorio)

---
+++ resources/header/check.php
@@ -0,0 +1,44 @@
+<div id="check-wrapper">
+	<div id="check-status">
+		<div id="status-img">
+			<i class="fa fa-user fa-5x" aria-hidden="true"></i>
+		</div>
+		<input id="status-text" type="text" class="form-control" placeholder="What's on your mind?">
+	</div>
+				
+	<div id="check-with">
+		<div class="input-group">
+			<span class="input-group-addon" id="basic-addon1">With</span>
+		  	<input type="text" class="form-control" placeholder="Who are you with?" aria-describedby="basic-addon1">
+		</div>
+	</div>
+	<div id="check-place">
+		<div class="input-group">
+			<span class="input-group-addon" id="basic-addon2">At</span>
+			<input type="text" class="form-control" placeholder="Where are you?" aria-describedby="basic-addon1">
+		</div>
+	</div>
+				
+	<div id="check-publish">
+		<i class="fa fa-user-plus" aria-hidden="true"></i>
+		<i class="fa fa-map-marker" aria-hidden="true"></i>
+		<div id="visibility-dropdown" class="dropdown pull-right">
+  			<button class="btn btn-default btn-sm dropdown-toggle" type="button" id="visibility-button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+      			Friends
+  	  			<span class="caret"></span>
+  			</button>
+  			<ul class="dropdown-menu" aria-labelledby="visibility-button">
+	    	  <li><a href="#">
+	    	  		<i class="fa fa-globe" aria-hidden="true"></i>Public
+	    	  	</a></li>
+	    	  <li><a href="#">
+	    	  		<i class="fa fa-user" aria-hidden="true"></i>Friends
+	    	  </a></li>
+	    	  <li><a href="#">
+	    	  		<i class="fa fa-lock" aria-hidden="true"></i>Only me
+	    	  </a></li>
+  			</ul>
+  			<button id ="post" type="button" class="btn btn-primary">Post</button>
+		</div>
+	</div>
+</div>
