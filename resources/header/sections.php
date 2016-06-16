0558eb2275b1a208e6b5c1a20ad89eca153cfec4
christianpanconi <panconi.christian@gmail.com>
2016-06-16 14:38:13 +0200

    Add files via upload

---
+++ resources/header/sections.php
@@ -0,0 +1,5 @@
+<nav id="sections-nav">
+	<div id="sections-suggestions-tab" class="sections-tab active">Suggestions</div>
+	<div id="sections-myprofile-tab" class="sections-tab">My Profile</div>
+	<div id="sectios-help-tab" class="sections-tab">Help</div>
+</nav>
