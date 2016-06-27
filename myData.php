8fa3ed6d51df6bb9b293075444790106390e990c
christianpanconi <panconi.christian@gmail.com>
2016-06-27 13:37:26 +0200

    Update myData.php

--- myData.php
+++ myData.php
@@ -36,29 +36,4 @@
 	}else{
 		echo "<h3>No Access Token</h3>";
 	}
-	
-	/*require_once( "config.php" );
-	
-	$fb = new Facebook\Facebook( array(
-		'app_id' => '511638992294104',
-		'app_secret' => 'd785a07c67ebb99933301a5e65dbaff8',
-		'default_graph_version' => 'v2.6'
-	) );
-	
-	// Send the request to Graph
-	try {
-	  $response = $fb->getClient()->sendRequest($request);
-	} catch(Facebook\Exceptions\FacebookResponseException $e) {
-	  // When Graph returns an error
-	  echo 'Graph returned an error: ' . $e->getMessage();
-	  exit;
-	} catch(Facebook\Exceptions\FacebookSDKException $e) {
-	  // When validation fails or other local issues
-	  echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	  exit;
-	}
-	
-	$graphNode = $response->getGraphNode();
-		
-	var_dump( $graphNode );*/
 ?>
