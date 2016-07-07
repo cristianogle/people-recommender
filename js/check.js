ae9ff8411289d88eb760bdf0a78a0048f5bdc44b
cristianogle <cristianogelli@gmail.com>
2016-07-07 10:48:05 +0200

    Update check.js

--- js/check.js
+++ js/check.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $("#user-icon").click(function(){
-    	console.log("click");
         $("#check-with").toggle();
     });
     $("#place-icon").click(function(){
         $("#check-place").toggle();
+    });
+    $("#tagged-p").click(function(){
+        $("#check-with").toggle();
     });
 });
