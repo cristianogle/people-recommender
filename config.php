a392c2924e65fdec3ec8e748c2b3c988629b2041
christianpanconi <panconi.christian@gmail.com>
2016-09-15 17:05:20 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,14 +5,14 @@
 	
 	require_once __DIR__.'/resources/libraries/facebook-php-sdk-v4-5.0.0/src/Facebook/autoload.php';
 	
-	define( 'APP_ID' , '511638992294104' );
-	define( 'APP_SECRET' , 'd785a07c67ebb99933301a5e65dbaff8' );
+	define( 'APP_ID' , '{app-id}' );
+	define( 'APP_SECRET' , '{app-secret}' );
 	define( 'AUTOLOAD_DIRECTORY' , 'resources/autoload');
 	
 	define( 'DB_SERVERNAME' , 'localhost' );
-	define( 'DB_USERNAME' , 'root' );
-	define( 'DB_PASSWORD' , 'mohawk');
-	define( 'DB_NAME' , 'pr_foursquare_global' );
+	define( 'DB_USERNAME' , '{db-user-name}' );
+	define( 'DB_PASSWORD' , '{db-password}');
+	define( 'DB_NAME' , '{db-name}' );
 	
 	spl_autoload_register( function($class){
 		$file = __DIR__.'/'.AUTOLOAD_DIRECTORY.'/'.$class.".php";
